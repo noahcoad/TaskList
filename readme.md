7d7b8f690871d8a99d9142a20c66044c92a4feb0
Noah Coad <noah@coad.net>
2019-11-06 21:19:26 -0600

    first iteration

---
+++ readme.md
@@ -0,0 +1,17 @@
+# Task List
+A Sublime 3 pluging
+
+Toggles a Task List icon in front of the line.
+Similar to "Toggle Comment", but cycles through emoji "icons"
+Command is called "Toggle Task List" and is mapped to `command + option + t` on mac by default
+
+Defaults:
+✡️ to do
+✅ done
+
+Edit task_list.sublime-settings to alter the emoji icons list to be used.
+
+I also like to use:
+✴️ highlight
+🅿️ informational
+🆘 warning!
---
+++ task_list.py
@@ -0,0 +1,53 @@
+#
+# Marks a line as a task item, and cycles through task icons
+#
+# https://github.com/noahcoad/task_list
+#
+
+import sublime, sublime_plugin
+import os.path
+
+class ToggleTaskListCommand(sublime_plugin.TextCommand):
+	def run(self, edit):
+		# load the list of emoji icons to be used for lists
+		icons = sublime.load_settings("task_list.sublime-settings").get('icons')
+
+		# go through each selection
+		for s in self.view.sel():
+			# go through each region backwards (so inserts don't change offset)
+			for r in self.view.lines(s)[::-1]:
+				# get the text for this line
+				t = self.view.substr(r)
+
+				# get the position of the first non-whitespace character
+				pos = r.begin() + next(iter(i for i,c in enumerate(t) if c != ' ' and c != '\t'), len(t))
+
+				# text from first non-white to end
+				line = self.view.substr(sublime.Region(pos, r.end()))
+
+				# check to see if these first characters match one of the task icons
+				icon_index = next(iter(index for index,icon in enumerate(icons) if len(line) >= len(icon) and line[0:len(icon)] == icon), -1)
+
+				# if the first characters match one of the icons, cycle to the next
+				if icon_index > -1:
+					# how many characters should be removed?
+					# also include space if there's one after the icon
+					char_count_to_erase = len(icons[icon_index]) + (1 if len(line) > len(icons[icon_index]) and line[len(icons[icon_index]):len(icons[icon_index]) + 1] == " " else 0)
+
+					# remove previous icon
+					self.view.erase(edit, sublime.Region(pos, pos + char_count_to_erase))
+
+					# if this isn't the last icon in the set, add the next one
+					if icon_index < len(icons) - 1:
+						self.view.insert(edit, pos, icons[icon_index + 1] + " ")
+
+				# insert the first icon
+				else:
+					self.view.insert(edit, pos, icons[0] + " ")
+
+
+# p.s. Yes, I'm using hard tabs for indentation.  bite me =P
+# set tabs to whatever level of indentation you like in your editor 
+# for crying out loud, at least they're consistent here, and use 
+# the ST3 command "Indentation: Convert to Spaces", which will convert
+# to spaces if you really need to be part of the 'soft tabs only' crowd =)
